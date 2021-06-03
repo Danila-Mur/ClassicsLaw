780b7dd6eeb827b8c1d292b3960ec6342a60cc86
Danil Murzin <danil.murzin.200026@gmail.com>
2021-06-03 18:00:05 +0300

    add contacts page

---
+++ src/pages/contacts/index.js
@@ -0,0 +1,8 @@
+import "./contacts.pug";
+import "./index.sass";
+
+import "./info";
+import "./communication";
+import "./contact";
+
+export default () => {};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import "./404";
 import initHome from "./home";
 import initFeedback from "./feedback";
 import initAboutCompany from "./about-company";
+import initContacts from "./contacts";
 
 export default () => {
   initHome();
   initFeedback();
   initAboutCompany();
+  initContacts();
 };
